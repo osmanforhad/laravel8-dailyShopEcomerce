d435729852ca9b2dd6d555d7a2d9c8a3a54fe12e
forhads <osmanforhad94@gmail.com>
2021-07-20 13:16:02 +0600

    write logice in admin auth middleware

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -16,6 +16,16 @@ class AdminAuth
      */
     public function handle(Request $request, Closure $next)
     {
+
+        if($request->session()->has('ADMIN_LOGIN')) {
+
+  
+
+        } else {
+            $request->session()->flash('error', 'Access Denaid');
+            return redirect('admin');
+        }
+
         return $next($request);
     }
 }
