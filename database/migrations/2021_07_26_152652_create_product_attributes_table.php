3e7806cccec396fce826de885b34105eb2b98fc0
forhads <osmanforhad94@gmail.com>
2021-07-26 21:32:55 +0600

    create product attribute migration

---
+++ database/migrations/2021_07_26_152652_create_product_attributes_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_attributes', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('product_id')->unsigned()->nullable();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->string('sku');
+            $table->string('image');
+            $table->string('mrp');
+            $table->string('price');
+            $table->integer('qty');
+            $table->bigInteger('size_id')->unsigned()->nullable();
+            $table->foreign('size_id')->references('id')->on('sizes')->onDelete('cascade');
+            $table->bigInteger('color_id')->unsigned()->nullable();
+            $table->foreign('color_id')->references('id')->on('colors')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_attributes');
+    }
+}
