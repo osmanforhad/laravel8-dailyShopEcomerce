974722f1cf3891c79373345be73fe8d49283bbc6
forhads <osmanforhad94@gmail.com>
2021-07-28 17:32:59 +0600

    include cistom 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Custom 404 error Page</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+<section style="padding-top:50px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2 text-center">
+                <h1 style="font-size:150px;">404</h1>
+                <hr>
+                <h2>Page not Found</h2>   
+                <p>The page you reguested could not found!</p>
+                <a href="/" class="btn btn-outline-primary">Go Back to Home</a> 
+            </div>            
+        </div>
+    </div>
+</section>
+
+</body>
+</html>
